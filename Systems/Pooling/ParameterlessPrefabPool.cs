8808dcda9b40c9d90b5b33f7df289827fc11c57c
virtushda <virtushda@gmail.com>
2024-08-27 20:00:56 -0400

    Added FetchInactive to ParameterlessPrefabPool

--- Systems/Pooling/ParameterlessPrefabPool.cs
+++ Systems/Pooling/ParameterlessPrefabPool.cs
@@ -18,6 +18,7 @@ namespace VLib
 
         public override GameObject CreateNewItem() => Object.Instantiate(prefab);
 
+        /// <summary> Depools and enables a prefab instance </summary>
         public override GameObject Fetch()
         {
             // Base impl already partially overridden to use CreateNewItem, we will get an object no matter what
@@ -25,6 +26,9 @@ namespace VLib
             obj.SetActive(true);
             return obj;
         }
+
+        /// <summary> Depools a prefab instance, but does not enable it for you </summary>
+        public GameObject FetchInactive() => base.Fetch();
 
         public override void Repool(GameObject prefabInstanceToPool)
         {
